d14e69a49aaf156e7862d3d2fc352cca77bed285
olzhasar <o.arystanov@gmail.com>
2020-09-13 01:35:52 +0600

    Added db init script

---
+++ db.py
@@ -0,0 +1,11 @@
+import os
+
+import sqlalchemy
+from databases import Database
+from dotenv import load_dotenv
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+db = Database(os.environ["DATABASE_URL"])
+metadata = sqlalchemy.MetaData()
