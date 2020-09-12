a735b1ecbf4763b7424a9653762800e03085312f
olzhasar <o.arystanov@gmail.com>
2020-09-13 02:03:53 +0600

    Added hash_password util. Designed tests

---
+++ tests/test_utils.py
@@ -0,0 +1,10 @@
+import pytest
+
+from utils import hash_password
+
+
+@pytest.mark.parametrize("password", ["123", "ASGIJASBOMAS", "90gjsjg0wu@!)($3gj3)"])
+def test_hash_password(password):
+    hashed = hash_password(password)
+    assert isinstance(hashed, str)
+    assert hashed != password
---
+++ utils.py
@@ -0,0 +1,7 @@
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed.decode("utf-8")
