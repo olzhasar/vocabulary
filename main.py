450fab6374d06273558975da7e76d6dc67b481e8
olzhasar <o.arystanov@gmail.com>
2020-09-13 23:50:43 +0600

    Added words api integration

--- main.py
+++ main.py
@@ -3,6 +3,17 @@ import uvicorn
 from app import app
 from models import User
 from schema import UserInSchema, UserOutSchema
+from words_api import WordsAPIClient
+
+
+@app.on_event("startup")
+def startup_event():
+    WordsAPIClient.get_session()
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    await WordsAPIClient.close_session
 
 
 @app.post("/user/")
@@ -15,6 +26,12 @@ async def create_user(user: UserInSchema):
 async def get_user(id: int):
     user = await User.get(id)
     return UserOutSchema(**user).dict()
+
+
+@app.get("/words/{word}")
+async def fetch_word(word: str):
+    result = await WordsAPIClient.query_word(word)
+    return result
 
 
 if __name__ == "__main__":
---
+++ words_api.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+
+import aiohttp
+from fastapi import HTTPException
+
+
+class WordsAPIClient:
+    sem: asyncio.Semaphore = None
+    session: aiohttp.ClientSession = None
+    base_url = "https://wordsapiv1.p.rapidapi.com"
+    headers = {
+        "x-rapidapi-host": "wordsapiv1.p.rapidapi.com",
+        "x-rapidapi-key": os.environ["WORDS_API_KEY"],
+    }
+
+    @classmethod
+    def get_session(cls) -> aiohttp.ClientSession:
+        if cls.session is None:
+            cls.session = aiohttp.ClientSession()
+        return cls.session
+
+    @classmethod
+    async def close_session(cls):
+        if cls.session:
+            await cls.session.close()
+            cls.session = None
+
+    @classmethod
+    async def query_word(cls, word: str):
+        session = cls.get_session()
+        url = f"{cls.base_url}/words/{word}"
+
+        try:
+            async with session.get(url, headers=cls.headers) as response:
+                if response.status != 200:
+                    raise HTTPException(404, "Word not found")
+
+                json_result = await response.json()
+        except Exception as e:
+            return {"ERROR": e}
+
+        return json_result
