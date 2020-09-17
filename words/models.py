6696ff912e7087bc9c09ae0ca06ea1c6565304b4
olzhasar <o.arystanov@gmail.com>
2020-09-18 01:54:17 +0600

    Removed migrations, words

--- words/models.py
+++
@@ -1,9 +0,0 @@
-from db import db, metadata
-from db import sqlalchemy as sa
-
-words = sa.Table(
-    "words",
-    metadata,
-    sa.Column("id", sa.Integer, primary_key=True, index=True),
-    sa.Column("word", sa.String(50), nullable=False, index=True, unique=True),
-)
