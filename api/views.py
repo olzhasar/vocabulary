29cf707857fca5ff988b6a1ea55445c282c2d4d1
olzhasar <o.arystanov@gmail.com>
2020-09-20 01:14:21 +0600

    Updated views

--- api/views.py
+++ api/views.py
@@ -3,9 +3,34 @@ from fastapi import APIRouter, FastAPI
 router = APIRouter()
 
 
-@router.get("/")
-async def index():
-    return "Test"
+@router.get("/token")
+async def login():
+    pass
+
+
+@router.get("/signup")
+async def signup():
+    pass
+
+
+@router.get("/words")
+async def word_list():
+    return "Not implemented"
+
+
+@router.get("/words/{word}")
+async def word_get(word: str):
+    return "Not implemented"
+
+
+@router.post("/words")
+async def word_add():
+    return "Not implemented"
+
+
+@router.delete("/words/{word}")
+async def word_remove(word: str):
+    return "Not implemented"
 
 
 def init_app(app: FastAPI):
