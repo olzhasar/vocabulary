e5878722ff3ad43e184ffad414e31eeb3f7570ed
olzhasar <o.arystanov@gmail.com>
2020-09-20 21:24:57 +0600

    Added word factory

--- db/tests/factories.py
+++ db/tests/factories.py
@@ -1,6 +1,15 @@
+import random
+import string
+
 import factory
 
-from db.models import User
+from db.models import User, Word
+
+
+def get_random_string(instance):
+    letters = string.ascii_lowercase
+    _range = range(random.randint(4, 20))
+    return "".join(random.choice(letters) for i in _range)
 
 
 class UserFactory(factory.Factory):
@@ -14,5 +23,19 @@ class UserFactory(factory.Factory):
     def _create(cls, model_class, *args, **kwargs):
         async def create_async(*args, **kwargs):
             return await model_class.register(*args, **kwargs)
+
+        return create_async(*args, **kwargs)
+
+
+class WordFactory(factory.Factory):
+    name = factory.LazyAttribute(get_random_string)
+
+    class Meta:
+        model = Word
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        async def create_async(*args, **kwargs):
+            return await model_class.create(*args, **kwargs)
 
         return create_async(*args, **kwargs)
