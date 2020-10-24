5be5aa32e7fc42ae00d546cd8ca12cbf09dd97fb
olzhasar <o.arystanov@gmail.com>
2020-10-24 22:08:59 +0600

    Added Search component

---
+++ frontend/src/components/Search.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="search">
+    <v-card elevation="2" class="mb-3">
+      <v-card-text>
+        <v-form @submit.prevent="searchWord" ref="form">
+          <v-text-field v-model="word" label="Word" required></v-text-field>
+          <v-btn color="info" class="mr-4" type="submit">
+            Search
+          </v-btn>
+        </v-form>
+      </v-card-text>
+    </v-card>
+    <v-card elevation="2" v-if="variants.length">
+      <v-card-title>
+        Results
+      </v-card-title>
+      <v-list-item v-for="variant in variants" :key="variant.id">
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ variant.definition }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ variant.part_of_speech }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+      <v-card-actions>
+        <v-btn color="info" text>Save</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import apiClient from "../api_client";
+
+export default Vue.extend({
+  name: "Search",
+  data: () => ({
+    word: null,
+    variants: []
+  }),
+  methods: {
+    searchWord: function() {
+      apiClient.get(`/search/${this.word}`).then(response => {
+        this.variants = response.data.variants;
+      });
+    }
+  }
+});
+</script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <v-container fluid fill-height>
+      <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md6>
+          <div class="text-h5 text-center mb-6">My vocabulary</div>
+          <search></search>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import search from "../components/Search.vue";
 
 export default Vue.extend({
+  data() {
+    return {
+      words: [],
+      searched: {
+        name: null,
+        data: {}
+      }
+    };
+  },
+  components: {
+    search
+  },
   methods: {
     loadWords: function() {
       this.$store.dispatch("getWords");
