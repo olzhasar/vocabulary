3691c2418bf1eb2a432afc0743f40e437d12b7aa
olzhasar <o.arystanov@gmail.com>
2020-10-24 19:00:33 +0600

    Api client redirect to login on 401

--- frontend/src/api_client/index.ts
+++ frontend/src/api_client/index.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import router from "../router";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:8000",
@@ -10,5 +11,16 @@ const token = localStorage.getItem("token");
 if (token) {
   apiClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 }
+
+apiClient.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  function(error) {
+    if (error.response.status === 401) {
+      router.push("/login");
+    }
+  }
+);
 
 export default apiClient;
